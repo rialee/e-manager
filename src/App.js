2fb22bd0344c3a41388a614fe6a3aa2e4eecfa9b
Victoria Lee <victoriacmlee@gmail.com>
2021-01-09 01:03:43 -0500

    set up pseudo code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// searchInput function () - component
+  // return JSX
+
+// resultDisplay function (props) - component
+  // props: users (filteredUsers)
+  // button to filter by name 
+    // onClick will this.sortedUsers
+
+  // return JSX
+
+// main app home page
+class App extends React.Component {
+
+  // state initial (schema for changes)
+  state ={
+    allUsers:[],
+    filteredUsers: [],
+    searchInput: "",
+    sortOption: ""
+  }
+
+  // component did mount
+    // call API funcition in utils folder
+      // set up new state (setState)
+        // allUsers: []
+        // filteredUsers: []
+
+  // handleInputChange 
+    // setState searchInput
+    // filter users by every letter entered in searchInput
+    // sort if relevant
+
+  // sortedUsers display on page
+    // use js to sort (by string, sortOptions: by name, DOB etc.)
+    // ascending/decending 
+
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
 }
 
 export default App;
